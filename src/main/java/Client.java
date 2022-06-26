5fc0d9a7534f9ea8467639d10126cb346af668d7
nmak1 <nmak1@rambler.ru>
2022-06-27 01:34:37 +0300

    initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 8089;
+        try (Socket client = new Socket(host, port);
+             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+            out.println("привет, сервер");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
