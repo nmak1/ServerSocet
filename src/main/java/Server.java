17cc9853d205c64145363b31ef372df86a9e495a
nmak1 <nmak1@rambler.ru>
2022-07-07 16:27:36 +0300

    Server.java close the stream

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,14 +5,13 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static java.lang.System.out;
-
 public class Server {
     public static void main(String[] args) throws IOException {
         int port = 8089;
-        while (true){
-            ServerSocket serverSocket = new ServerSocket(port);
-            try (Socket clientSocket = serverSocket.accept()) {
+        while (true) {
+
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept()) {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 System.out.println("New connection accepted");
